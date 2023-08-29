7b48d49b8f2dbe823f85955dc1d5b6be69d049c9
sachin0884 <sachin.tatkare1984@gmail.com>
2023-08-29 11:03:38 +0530

    testng package added

--- src/main/java/com/qa/ecommerce/listeners/TestAllureListener.java
+++ src/main/java/com/qa/ecommerce/listeners/TestAllureListener.java
@@ -1,15 +1,17 @@
 package com.qa.ecommerce.listeners;
 
 
-import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.*;
 
 import com.qa.ecommerce.factory.DriverFactory;
+
+import io.qameta.allure.Attachment;
 
 
 public class TestAllureListener implements ITestListener {
